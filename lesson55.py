bc065539ac538a03353b4475f7421830b008443d
dashingcoderrohan123 <anandrao23046@gmail.com>
2024-12-20 23:38:39 -0800

    Add files via upload

---
+++ lesson55.py
@@ -0,0 +1,11 @@
+
+age=int(input("enter the age"))
+
+
+if age < 0:
+    print("Sorry,please enter the correct age")
+else:
+    if age%2==0:
+        print("the age entered is even")
+    else:
+        print("The age entered is odd")    
